1dfaff03c1f1a395738e6b80132016b3ef1fb592
gladraul <liuxianda@sia.cn>
2017-02-20 10:36:13 +0800

    深度优先搜索

--- Acm-poj/src/poj/Poj1014_01.java
+++ Acm-poj/src/poj/Poj1014_01.java
@@ -3,72 +3,75 @@ package poj;
 import java.util.Scanner;
 
 /**
- * Dividing问题
- * 描述：
- *   （1）分割大理石为相同的两份，每个大理石都有相应的价值，1~6
- * 输入：
- *   （1）n1 n2 n3 n4 n5 n6: ni表示价值i的大理石个数
- *   （2）0 0 0 0 0 0: 表示输入结束
- * 输出：
- *   （1）Collection #k:表示k组输入
- *   （2）存在两种结果：  "Can be divided." 或 "Can't be divided."
- *   （3）每组输出空一行
- * 思路：
- *   （1）输入总价值为偶数；
- *   （2）每次迭代，用总价值的一半减去较大价值的大理石的价值
  * @author liuxd
  *
  */
 public class Poj1014_01 {
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		int k = 0;
-		while(input.hasNext()) {
-			k++;
+    private static int[] aValNum;
+    private static boolean flag = false;
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int k = 0;
+	while(input.hasNext()) {
+            k++;
+            aValNum = new int[6];
+            String[] aInput = input.nextLine().split(" ");
+            int sum = 0;
+            for(int i = 0; i < 6; i++) {
+                aValNum[i] = Integer.parseInt(aInput[i]);
+                sum += aValNum[i] * (i + 1);
+            }
+
+            if(sum == 0) {
+                break;
+            }
+            
+            int need = (sum % 2 == 0 ? sum / 2 : -1);		
+            System.out.println(new StringBuilder("Collection #").append(k).append(":"));
+            if(need == -1) {
+                System.out.println("Can't be divided.");
+            }
+            else { 
+                flag = false;
+                dfs(need, 5);
+                if(flag == true) {
+                    System.out.println("Can be divided.");
+                }
+                else {
+                    System.out.println("Can't be divided.");
+                }
+            }
 			
-			int[] aValue = new int[6];
-			String[] aInput = input.nextLine().split(" ");
-			int sum = 0;
-			for(int i = 0; i < 6; i++) {
-				aValue[i] = Integer.parseInt(aInput[i]);
-				sum += aValue[i] * (i + 1);
-			}
-			
-			if(sum == 0) {
-				break;
-			}
-			
-			if(sum % 2 != 0) {
-				System.out.println(new StringBuilder("Collection #").append(k).append(":"));
-				System.out.println("Can't be divided.");
-			}
-			else {
-				int half = sum / 2;
-				int max = 5;
-				while(true) {
-					while(max >= 0 
-							&& ((max + 1) > half || aValue[max] == 0)) {
-						max--;
-					}
-					if(max < 0) {
-						System.out.println(new StringBuilder("Collection #").append(k).append(":"));
-						System.out.println("Can't be divided.");
-						break;
-					}
-					else {
-						half = half - (max + 1);
-						aValue[max]--;
-					}
-					
-					if(half == 0) {
-						System.out.println(new StringBuilder("Collection #").append(k).append(":"));
-						System.out.println("Can be divided.");
-						break;
-					}
-				}
-			}
-			
-			System.out.println();
-		}
-	}
+            System.out.println();
+        }
+    }
+
+    private static void dfs(int need, int index) {
+        if(need == 0) {
+            flag = true;
+            return;
+        } 
+        
+        if(flag == true) {
+            return;
+        }
+
+        for(int i = index; i > 0; i--) {
+            if(aValNum[i] > 0) {
+                if(need >= i + 1) {
+                    aValNum[i]--;
+                    dfs(need - i - 1, i);
+                    
+                    if(flag == true) {
+                        return;
+                    }
+                }
+                else {
+                    i = need;
+                }
+            }        
+        }
+
+    }
 }

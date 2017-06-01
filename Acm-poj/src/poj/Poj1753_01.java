a41d5115203a4f82812e2a01b392f9c661dda33a
gladraul <liuxianda@sia.cn>
2017-06-01 13:45:47 +0800

    枚举算法

--- Acm-poj/src/poj/Poj1753_01.java
+++ Acm-poj/src/poj/Poj1753_01.java
@@ -4,158 +4,133 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 /**
- * @author liuxd
+ * Enumeration Algorithm
  *
+ * @author liuxd
  */
 public class Poj1753_01 {
 
-    public static void main(String[] args) {        
-        Scanner input = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 
-        while(true) {
-            char[] arrValue = new char[16]; 
-            for(int i = 0; i < 4; i++) {
-                if(input.hasNextLine()) {
-                    String tmp = input.next();
-                    for(int j = 0; j < 4; j++) {
-                        arrValue[4 * i + j] = tmp.charAt(j);
-                    }
-                }
-            }
+		while(true) {
+			char[] arrValue = new char[16];
+			for(int i = 0; i < 4; i++) {
+				if(input.hasNextLine()) {
+					String tmp = input.next();
+					for(int j = 0; j < 4; j++) {
+						arrValue[4 * i + j] = tmp.charAt(j);
+					}
+				}
+			}
 
-            if(isGoal(arrValue)) {
-                System.out.println("0");
-                continue;
-            }
-            
-            //w
-            char[] arrW = Arrays.copyOf(arrValue, 16);    
-            int stepW = flipArray(arrW, 'w');
-            if(!isGoal(arrW)) {
-                stepW = 17;
-            }
-           
-            //b
-            char[] arrB = Arrays.copyOf(arrValue, 16);    
-            int stepB = flipArray(arrB, 'b');
-            if(!isGoal(arrB)) {
-                stepB = 17;
-            }
-            
-            if(stepW != 17 || stepB != 17) {
-                System.out.println(min(stepW, stepB));    
-            }
-            else {
-                System.out.println("Impossible");
-            }
-        }    
-    }
+			if(isGoal(arrValue)) {
+				System.out.println("0");
+				continue;
+			}
 
-    //判断是否是目标状态
-    private static boolean isGoal(char[] arr) {
-        for(int i = 1; i < arr.length; i++) {
-            if(arr[i] != arr[0]) {
-                return false;
-            }
-        }
-        return true;
-    }
+			int num = 0;
+			for(num = 1; num < 17; num++) {
+				int[] result = new int[num];
+				if(flipByCombn(arrValue, 0, result, num, num)) {
+					System.out.println(num);
+					break;
+				}
+			}
 
-    //翻转字符
-    private static char flip(char c) {
-        if(c == 'w') {
-            return 'b';
-        }
-        else {
-            return 'w';
-        }
-    }
+			if(num == 17) {
+				System.out.println("Impossible");
+			}
+		}
+	}
 
-    //按照字符c翻转数组arr
-    private static int flipArray(char[] arr, char c) {
-        int step = 0;//达到全字符c的步数
-        if(arr[0] != c) {
-            arr[0] = c;
-            arr[1] = flip(arr[1]);
-            arr[4] = flip(arr[4]);
-            step++; 
-        }
-            
-        //位置5为起点
-        for(int i = 5; i < 16; i++) {
-            switch(i) {
-            case 8:
-                if(arr[4] != c) {
-                    arr[4] = c;
-                    arr[8] = flip(arr[8]);
-                    arr[9] = flip(arr[9]);
-                    arr[12] = flip(arr[12]);
-                    step++;
-                }
-                break;
+	/**
+	 * 使用组合的方式翻转数组
+	 * @param arr 原始数组
+	 * @param start 起始位置
+	 * @param result 保留从原始数组中取出元素的索引
+	 * @param count 待取出元素的数目
+	 * @param num 组合元素总数目
+	 */
+	private static boolean flipByCombn(char[] arr, int start, int[] result, int count, int num) {
+		for(int i = start; i < arr.length - count + 1; i++) {
+			result[count - 1] = i;
+			if(count - 1 == 0) {
+				char[] tmpArray = Arrays.copyOf(arr, 16);
+				for(int j = num - 1; j >= 0; j--) {
+					flipArray(tmpArray, result[j]);
+					//System.out.print(result[j] + "\t");
+				}
+				//System.out.println();
+				if(isGoal(tmpArray)) {
+					return true;
+				}
+			}
+			else {
+				if(flipByCombn(arr, i + 1, result, count - 1, num)) {
+					return true;
+				}
+			}
+		}
 
-            case 7:
-            case 11:
-                if(arr[i - 4] != c) {
-                    arr[i - 4] = c;
-                    arr[i] = flip(arr[i]);
-                    arr[i - 1] = flip(arr[i - 1]);
-                    arr[i + 4] = flip(arr[i + 4]);
-                    step++;
-                }
-                break;
+		return false;
+	}
 
-            case 12:
-                if(arr[8] != c) {
-                    arr[8] = c;
-                    arr[12] = flip(arr[12]);
-                    arr[13] = flip(arr[13]);
-                    step++;
-                }
-                break;
+	/**
+	 * 将数组按位置翻转
+	 * @param arr
+	 * @param index
+	 */
+	private static void flipArray(char[] arr, int index) {
+		if(index - 1 >= 0
+		&& (index - 1) % 4 != 3) {
+			arr[index - 1] = flipChar(arr[index - 1]);
+		}
 
-            case 13:
-            case 14:
-                if(arr[i - 4] != c) {
-                    arr[i - 4] = c;
-                    arr[i] = flip(arr[i]);
-                    arr[i - 1] = flip(arr[i - 1]);
-                    arr[i + 1] = flip(arr[i + 1]);
-                    step++;
-                }
-                break;
+		arr[index] = flipChar(arr[index]);
 
-            case 15:
-                if(arr[11] != c) {
-                    arr[11] = c;
-                    arr[15] = flip(arr[15]);
-                    arr[14] = flip(arr[14]);
-                    step++;
-                }
-                break;
+		if(index + 1 <= 15
+		&& (index + 1) % 4 != 0) {
+			arr[index + 1] = flipChar(arr[index + 1]);
+		}
 
-            default:
-                if(arr[i - 4] != c) {
-                    arr[i - 4] = c;
-                    arr[i - 1] = flip(arr[i - 1]);
-                    arr[i] = flip(arr[i]);
-                    arr[i + 1] = flip(arr[i + 1]);
-                    arr[i + 4] = flip(arr[i + 4]);
-                    step++;
-                }
+		if(index - 4 >= 0
+		&& index / 4 > 0) {
+			arr[index - 4] = flipChar(arr[index - 4]);
+		}
 
-                break;
-            }
-                
-        }
-        return step;
-    } 
+		if(index + 4 <= 15
+		&& index / 4 <  3) {
+			arr[index + 4] = flipChar(arr[index + 4]);
+		}
+	}
 
-    //返回较小值
-    private static int min(int a, int b) {
-        if(a <= b) {
-            return a;
-        }
-        return b;
-    }
+	/**
+	 * 判断是否是目标状态
+	 * @param arr
+	 * @return
+	 */
+	private static boolean isGoal(char[] arr) {
+		for(int i = 1; i < arr.length; i++) {
+			if(arr[i] != arr[0]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 *
+	 * 翻转字符
+	 * @param c
+	 * @return
+	 */
+	private static char flipChar(char c) {
+		if(c == 'w') {
+			return 'b';
+		}
+		else {
+			return 'w';
+		}
+	}
 }
